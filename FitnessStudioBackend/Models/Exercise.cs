2ebcfe9ffb691fc9253fce2c7e0e63d08afcddaa
Tilen Kricaj <t.kricaj@ncahin.com>
2024-04-28 21:00:05 +0200

    feat:Add Exercise adn Exercise Group classes and clean up the default api

---
+++ FitnessStudioBackend/Models/Exercise.cs
@@ -0,0 +1,12 @@
+﻿namespace FitnessStudioBackend.Models
+{
+    public class Exercise
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public int? ExerciseGroupId { get; set; }
+        public ExerciseGroup? ExerciseGroup { get; set; }
+
+     
+    }
+}
