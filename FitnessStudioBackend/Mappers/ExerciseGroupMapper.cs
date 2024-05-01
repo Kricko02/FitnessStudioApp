70a7acb1fc8278f32f995a949e8c6f0427cef707
Tilen Kricaj <t.kricaj@ncahin.com>
2024-05-01 14:53:11 +0200

    feat:include

---
+++ FitnessStudioBackend/Mappers/ExerciseGroupMapper.cs
@@ -0,0 +1,21 @@
+﻿using FitnessStudioBackend.Dtos.Exercise;
+using FitnessStudioBackend.Dtos.ExerciseGroup;
+using FitnessStudioBackend.Models;
+
+namespace FitnessStudioBackend.Mappers
+{
+    public static class ExerciseGroupMapper
+    {
+        public static ExerciseGroupDto ToExerciseGroupDto(this ExerciseGroup exerciseGroupModel)
+        {
+            return new ExerciseGroupDto
+            {
+                Id = exerciseGroupModel.Id,
+                Name = exerciseGroupModel.Name,
+                Exercises = exerciseGroupModel.Exercises.Select(c=>c.ToExerciseDto()).ToList()
+            };
+
+            
+        }
+    }
+}
---
+++ FitnessStudioBackend/Repository/ExerciseGroupRepository.cs
@@ -0,0 +1,27 @@
+﻿using FitnessStudioBackend.Data;
+using FitnessStudioBackend.Interfaces;
+using FitnessStudioBackend.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace FitnessStudioBackend.Repository
+{
+    public class ExerciseGroupRepository:IExerciseGroupRepository
+    {
+        private readonly ApplicationDBContext _context;
+
+        public ExerciseGroupRepository(ApplicationDBContext context)
+        {
+            _context = context;
+        }
+
+        public Task<bool> ExerciseGroupExists(int id)
+        {
+           return _context.Exercise.AnyAsync(s => s.Id == id);
+        }
+
+        public async Task<List<ExerciseGroup>> GetAllAsync()
+        {
+            return await _context.ExerciseGroup.Include(c => c.Exercises).ToListAsync();
+        }
+    }
+}
