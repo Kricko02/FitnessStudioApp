83e437ea022c4431f1968ea87c8373652fb663d1
Tilen Kricaj <t.kricaj@ncahin.com>
2024-05-01 17:23:52 +0200

    feat:Data validation

--- FitnessStudioBackend/Dtos/Exercise/RequestExerciseDto.cs
+++ FitnessStudioBackend/Dtos/Exercise/RequestExerciseDto.cs
@@ -1,7 +1,13 @@
-﻿namespace FitnessStudioBackend.Dtos.Exercise
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FitnessStudioBackend.Dtos.Exercise
 {
     public class RequestExerciseDto
     {
+        [Required]
+        [MinLength(5,ErrorMessage ="Name must be atleast 5 characters long")]
+        [MaxLength(50, ErrorMessage = "Name cannot be over 50 characters ")]
+
         public string Name { get; set; } = string.Empty;
     }
 }
