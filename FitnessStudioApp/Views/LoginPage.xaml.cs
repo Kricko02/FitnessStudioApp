ccc1d3432b7647b177e4f855dfd74fd86444847c
Verdev420 <tjan4.vinder4@gmail.com>
2024-05-15 21:36:05 +0200

    Added icons and password show feature

--- FitnessStudioApp/Views/LoginPage.xaml.cs
+++ FitnessStudioApp/Views/LoginPage.xaml.cs
@@ -8,4 +8,17 @@ public partial class LoginPage : ContentPage
 		
 	}
 
+    private void tb_password_TrailingIconClicked(object sender, EventArgs e)
+    {
+		if (tb_password.TrailingIconData== IconPacks.IconKind.Material.VisibilityOff)
+		{
+			tb_password.InputType = Material.Components.Maui.Primitives.InputType.SingleLine;
+			tb_password.TrailingIconData = IconPacks.IconKind.Material.Visibility;
+        }
+		else 
+		{
+			tb_password.InputType = Material.Components.Maui.Primitives.InputType.Password;
+			tb_password.TrailingIconData = IconPacks.IconKind.Material.VisibilityOff;
+        }
+    }
 }
